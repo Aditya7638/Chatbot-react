65b4bb2aa800babd810e43b06c12473bb382fef2
Adita7638 <adityabhuria29@gmail.com>
2026-08-22 19:33:12 +0530

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,6 +2,13 @@ import { Header } from "../components/Header";
 import { products } from "../../startingcode/ecommerce-project-main/data/products";
 import "./HomePage.css";
 export function HomePage() {
+
+  fetch('http://localhost:3000/api/products')
+    .then((response) =>{
+      return response.json()
+      }).then((data) =>{
+        console.log(data);
+    })
   return (
     <>
       <title>Ecommerce Website</title>
