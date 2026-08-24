865ed9bb1fc7875c6c9d5046f0a9b30aeef33321
Adita7638 <adityabhuria29@gmail.com>
2026-08-24 19:14:37 +0530

    Lift the cart upward so that only need to load the cart once and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,17 +1,28 @@
+import axios from 'axios';
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/Checkout/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { useState, useEffect } from 'react';
 import { TrackingPage } from './pages/TrackingPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import './App.css'
 
 function App() {
+
+  const [cart, setCart] = useState([]);
+  useEffect(() =>{
+      axios.get("/api/cart-items")
+      .then((response) => {
+        setCart(response.data)
+      });
+  }, []);
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route index element={<HomePage cart={cart}/>} />
       <Route path="orders" element={<OrdersPage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
